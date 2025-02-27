be0b90fd93eb095ea7fd5e3b1d9c1c8d84a210ee
Urban Vidovič <urbanfoundit@gmail.com>
2025-02-27 13:29:35 +0100

    feat(raycast): add script for clearing downloads folder
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

---
+++ raycast/scripts/clear-downloads.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Clear Downloads
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Downloads Cleaner
+
+# Documentation:
+# @raycast.description Removes everything in the Downloads folder.
+# @raycast.author pseudobun
+# @raycast.authorURL https://pseudobun.dev
+
+rm -rf ~/Downloads/*
